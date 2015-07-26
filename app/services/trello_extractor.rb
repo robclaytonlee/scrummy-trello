a5729cbc5572b8a350e572e84a1dd3f733180a30
rob.lee <rob.lee@squerb.com>
2015-07-26 14:43:44 -0600

    Added url to the api call to get card URLs.  Added logic to extract card points from labels

--- app/services/trello_extractor.rb
+++ app/services/trello_extractor.rb
@@ -6,8 +6,25 @@ class TrelloExtractor
                    "token=#{Rails.application.secrets.trello_member_token}"].join('&')
 
   def extract_cards(trello_board_id)
-    url = "https://api.trello.com/1/board/#{trello_board_id}/cards?fields=name,labels&#{@@auth_params}"
+    url = "https://api.trello.com/1/board/#{trello_board_id}/cards?fields=name,labels,url&#{@@auth_params}"
     response = RestClient.get url, { accept: :json }
     JSON.parse(response)
+  end
+
+  def import_cards(trello_cards)
+    n = 0
+    trello_cards.each do |element|
+      c = Card.find_or_initialize_by(trello_id: element["id"])
+
+      # Compute points from labels which are assumed to be numeric
+      points = 0
+      element["labels"].each do |label|
+        points += label["name"].to_f
+      end
+
+      c.update(name: element["name"], board_id: 4, trello_url: element["url"], points: points)
+      n+=1
+      break unless n < 20
+    end
   end
 end
