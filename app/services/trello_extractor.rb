50561474c0d9dec070f62ad8467769123efec26b
rob.lee <rob.lee@squerb.com>
2015-07-30 16:39:38 -0600

    Made changes to account for the new List resource.  A Board has many lists and a list has many cards.  A card must be on a list and a list must be on a board.  Added code to extract and import lists into the rails app.  Added instance variables to hold the board_id, trello_board_id, and a list has to easily look up the list_id given the trello_list_id.  Refactored the code in general, and removed hardcoded limits used in testing.

--- app/services/trello_extractor.rb
+++ app/services/trello_extractor.rb
@@ -2,37 +2,67 @@
 
 class TrelloExtractor
 
+  attr_reader :board_id, :trello_board_id, :lists
+
   @@auth_params = ["key=#{Rails.application.secrets.trello_dev_public_key}",
                    "token=#{Rails.application.secrets.trello_member_token}"].join('&')
 
-  def trello_cards(board_id)
-    trello_board_id = nil
+  def initialize(board_id)
+    @board_id = board_id
+    @trello_board_id = Board.where(id: board_id).select(:trello_id).first.trello_id
+    @lists = {}
+  end
 
-    #Get the trello id for the board
-    Board.where(id: board_id).select(:trello_id) do |b|
-      trello_board_id = b.trello_id
-    end
-
-    url = "https://api.trello.com/1/board/#{trello_board_id}/cards?fields=name,labels,url,idShort&#{@@auth_params}"
+  def trello_lists
+    url = "https://api.trello.com/1/boards/#{@trello_board_id}/lists/open?cards=none&#{@@auth_params}"
     response = RestClient.get url, { accept: :json }
     JSON.parse(response)
   end
 
-  def import_cards(trello_cards, board_id)
+  def sync_lists
     n = 0
-    trello_cards.each do |element|
-      c = Card.find_or_initialize_by(trello_id: element["id"])
 
-      # Compute points from labels which are assumed to be numeric
-      points = 0
-      element["labels"].each do |label|
-        points += label["name"].to_f
-      end
-
-      c.update(name: element["name"], board_id: board_id, short_id: element["idShort"], trello_url: element["url"], points: points)
+    t_lists = trello_lists
+    t_lists.each do |element|
+      l = List.find_or_initialize_by(trello_id: element["id"])
+      l.update(name: element["name"], board_id: @board_id, closed: element["closed"], position: element["pos"])
       n+=1
-      break unless n < 20
+      # Initialize a Hash for easily getting the list id given the trello list id
+      @lists[element["id"]] = l.id
     end
     n
   end
+
+  def trello_cards
+    url = "https://api.trello.com/1/board/#{@trello_board_id}/cards?fields=name,labels,url,idShort,idList&#{@@auth_params}"
+    response = RestClient.get url, { accept: :json }
+    JSON.parse(response)
+  end
+
+  def sync_cards
+    n = 0
+
+    if @lists.count == 0 then sync_lists end
+    t_cards = trello_cards
+    t_cards.each do |element|
+      c = Card.find_or_initialize_by(trello_id: element["id"])
+      pts = points(element["labels"])
+      list_id = @lists[element["idList"]]
+      c.update(name: element["name"], board_id: @board_id, list_id: list_id, short_id: element["idShort"], trello_url: element["url"], points: pts)
+      n+=1
+    end
+    n
+  end
+
+  def points(labels = [])
+    pts = 0
+
+    # Compute points from labels which are assumed to be numeric
+    labels.each do |l|
+      pts += l["name"].to_f
+    end
+    pts
+  end
+  private :points
+
 end
